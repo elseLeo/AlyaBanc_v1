2af7baca3bbfd49275fb84154eab20d635dd9d1e
Leonardo <114580366+elseLeo@users.noreply.github.com>
2022-10-13 12:58:46 -0300

    Add files via upload

---
+++ AlyaBank/src/dados/Conta.java
@@ -0,0 +1,82 @@
+package dados;
+
+public class Conta {
+	String nome, CPF, profissao, rua, cidade, UF;
+	int nCasa;
+	double renda;
+	/*CONSTRUTOR*/
+	public Conta(String nome, String CPF, String profissao, double renda, String rua, int nCasa, String cidade, String UF) {
+		this.nome = nome;
+		this.CPF = CPF;
+		this.profissao = profissao;
+		this.rua = rua;
+		this.cidade = cidade;
+		this.UF = UF;
+		this.nCasa = nCasa;
+		this.renda = renda;
+	}
+	/*GETERS E SETERS*/
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setCPF(String CPF) {
+		this.CPF = CPF;
+	}
+
+	public String getCPF() {
+		return CPF;
+	}
+
+	public void setProfissao(String profissao) {
+		this.profissao = profissao;
+	}
+
+	public String getProfissao() {
+		return profissao;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setUF(String UF) {
+		this.UF = UF;
+	}
+
+	public String getUF() {
+		return UF;
+	}
+
+	public void setNCasa(int nCasa) {
+		this.nCasa = nCasa;
+	}
+
+	public int getNCasa() {
+		return nCasa;
+	}
+
+	public void setRenda(double renda) {
+		this.renda = renda;
+	}
+
+	public double getRenda() {
+		return renda;
+	}
+}
---
+++ AlyaBank/src/menu/auxiliar.java
@@ -0,0 +1,26 @@
+package menu;
+
+public class auxiliar {
+	/* CLASE DE MENUS */
+
+	public void menuInicial() {
+		System.out.println("============AlyaBank============");
+		System.out.println("1 - Criar conta");
+		System.out.println("2 - Logar");
+		System.out.println("0 - Sair");
+		System.out.println("================================");
+	}
+
+	public void menuPrincipal() {
+		System.out.println("============AlyaBank============");
+		System.out.println("1 - Depositar");
+		System.out.println("2 - Sacar");
+		System.out.println("3 - Transferir");
+		System.out.println("4 - Saldo");
+		System.out.println("5 - Pagamento");
+		System.out.println("0 - Sair");
+		System.out.println("================================");
+
+	}
+
+}
