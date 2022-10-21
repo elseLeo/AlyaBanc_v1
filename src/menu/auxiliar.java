497b3f0e9b524f00195fc1430d16a03b7a7a785a
Leonardo <114580366+elseLeo@users.noreply.github.com>
2022-10-20 21:13:30 -0300

    Add files via upload

---
+++ src/menu/auxiliar.java
@@ -0,0 +1,33 @@
+package menu;
+
+public class auxiliar {
+	/* CLASE DE MENUS */
+
+	public void menuInicial() {
+		System.out.println("============AlyaBank============");
+		System.out.println("1 - Criar conta");
+		System.out.println("2 - Logar");
+		System.out.println("0 - Sair");
+		System.out.println("================================");
+	}
+
+	public void menuPrincipal() {
+		System.out.println("============AlyaBank============");
+		System.out.println("1 - Depositar");
+		System.out.println("2 - Sacar");
+		System.out.println("3 - Transferir");
+		System.out.println("4 - Saldo");
+		System.out.println("5 - Pagamento");
+		System.out.println("6 - Sparkles");
+		System.out.println("0 - Sair");
+		System.out.println("================================");
+
+	}
+	public void menuPontos() {
+		System.out.println("============AlyaBank============");
+		System.out.println("1 - Consultar Sparkles");
+		System.out.println("2 - Resgatar Sparkles");
+		System.out.println("================================");
+	}
+
+}
